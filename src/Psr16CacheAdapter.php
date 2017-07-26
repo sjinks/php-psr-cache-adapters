a1c361dcbea943e69dd44e021390708841dc2191
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-07-26 19:55:17 +0300

    DateInterval => \DateInterval

--- src/Psr16CacheAdapter.php
+++ src/Psr16CacheAdapter.php
@@ -44,7 +44,7 @@ class Psr16CacheAdapter implements \Psr\SimpleCache\CacheInterface
      *
      * @param string                $key   The key of the item to store.
      * @param mixed                 $value The value of the item to store, must be serializable.
-     * @param null|int|DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     * @param null|int|\DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
      *                                     the driver supports TTL then the library may set a default value
      *                                     for it or let the driver take care of that.
      *
